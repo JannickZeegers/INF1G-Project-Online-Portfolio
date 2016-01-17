89a68bac5256c7b5d28bcbeffc0cea82b0f4e13a
JannickZeegers <JannickZeegers@gmail.com>
2016-01-17 16:42:56 +0100

    Mailsend.php bereikbaar via mailinbox.php
    scrubs

--- include/mailinbox.php
+++ include/mailinbox.php
@@ -21,7 +21,6 @@ include_once 'portfolio.php';
             if(isset($_SESSION['user']))
             {   
                     $messages = portfolio_get_user_messages($_SESSION['user']['gebruikersId']);
-                    
                     echo "<h3>Inbox:</h3>";
                     echo "<table class='tableLeft' width='100%' border='1'>";
                     echo "<tr><th>Zender</th><th>Onderwerp</th></tr>";
@@ -55,7 +54,9 @@ include_once 'portfolio.php';
                         echo "<tr><td>{$o['voornaam']} {$o['achternaam']}</td>";
                         echo "<td><a href='mailview.php?mail=" . $m['berichtId'] . "'>{$m['onderwerp']}</a></td></tr>"; 
                     }
-                    echo "</table>";
+                    echo "</table>"; 
+                    echo "<p><hr></p>";
+                    echo "<h3><a href='mailsend.php'> Nieuw bericht opstellen</a></h3>";
             }           
                         
             ?>
