66e5f3a5aca635afd47f8f337d6cb1cecb12c3cd
colacan <wrdbrookhuis@gmail.com>
2016-01-24 22:42:55 +0100

    Guestbookhandler update
    - Added 'delete' command

--- include/guestbookhandler.php
+++ include/guestbookhandler.php
@@ -1,11 +1,12 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-include 'portfolio.php';
+header("Access-Control-Allow-Origin: *");   //Needed because we will be accessing this from another (sub)domain
+include 'portfolio.php';                    //Required for database connection and table name constants
 
 $action = filter_input(INPUT_POST, 'action');
-//$params = filter_input(INPUT_POST, 'params');
-//$action = filter_input(INPUT_GET, 'action');
-
+/*
+ * This is a php page that handles guestbook interaction via post requests.
+ * Returns json encoded stuff.
+ */
 switch($action)
 {
     case 'get':{
@@ -18,7 +19,7 @@ switch($action)
                 if($result){
                     $messageArray = array();
                     while(($row = mysqli_fetch_assoc($result))){
-                        $row['bericht'] = htmlentities($row['bericht']);
+                        $row['bericht'] = $row['bericht'];
                         array_push($messageArray, $row);
                     }
                     echo json_encode($messageArray);
@@ -39,7 +40,7 @@ switch($action)
                 if(strlen($name) <= 60 && strlen($msg) <= 700){
                     $link = portfolio_connect();
                     if($link){
-                        $sql = "INSERT INTO " . TABLE_GUESTBOOK . " (berichtID, zendernaam, email, bericht, ontvangerID) VALUES(NULL, '"
+                        $sql = "INSERT INTO " . TABLE_GUESTBOOK . " (berichtId, zendernaam, email, bericht, ontvangerId) VALUES(NULL, '"
                                 . mysqli_real_escape_string($link, $name) . "', '"
                                 . mysqli_real_escape_string($link, $mail) . "', '"
                                 . mysqli_real_escape_string($link, $msg) . "', "
@@ -64,8 +65,27 @@ switch($action)
         }
         break;
     }
+    /*
+     * Untested!
+     */
+    case 'delete':{
+        $messageId = filter_input(INPUT_POST, 'messageId', FILTER_VALIDATE_INT);
+        if($messageId){
+            $link = portfolio_connect();
+            if($link){
+                $sql = "DELETE FROM " . TABLE_GUESTBOOK . " WHERE berichtId=" . mysqli_real_escape_string($link, $messageId);
+                $result = mysqli_query($link, $sql);
+                if($result){
+                    echo json_encode('success');
+                    break;
+                }
+            }
+        }
+        echo json_encode('error');
+        break;
+    }
     default:{
-        echo json_encode('default');
+        echo json_encode('unknown');
         break;
     }
 }
